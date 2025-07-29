632c70db56a074699335853bfe96976a3d57ee18
tmcgahey93 <tmcgahey93@gmail.com>
2025-07-29 12:48:52 -0400

    Initial Commit

---
+++ src/main/java/com/example/flink-fraud-detection/App.java
@@ -0,0 +1,13 @@
+package com.example.flink-fraud-detection;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
